21433b6c6336e662a9409cd986a1c2ecb4784afb
Greg Silverman <Greg Silverman@gbshpwindows7>
2014-12-19 15:42:06 -0800

    start

---
+++ src/Hashtable/Hashtable.java
@@ -0,0 +1,13 @@
+package Hashtable;
+
+import java.lang.System;
+
+
+public class Hashtable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
