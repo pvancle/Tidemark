7007cc12251c781e183bb8224e911e4664f9465d
Greg Silverman <Greg Silverman@gbshpwindows7.hsd1.ca.comcast.net>
2014-12-21 17:57:23 -0800

    Move performance to its own file.

--- src/FHTP/FHTest.java
+++ src/FHTP/FHTest.java
@@ -114,75 +114,6 @@ public class FHTest {
 	}
 	
 	/*
-	 * Compares the performance time of FastHashtable to java.util.Hashtable.
-	 */
-	public class TestRelativeComputationTime extends TestAbstract {
-		public boolean run() {
-			
-			final int N = 1<<16;
-			final int K = 8;
-			long s = 0;
-			Date start_time = null;
-			Date stop_time  = null;
-			double ft_total_time = 0;
-			double ht_total_time = 0;
-			//...
-			//...initialize both FastHashtable & Hashtable
-			//...
-			FastHashtable ft = new FastHashtable(1024,5.0f);
-			Hashtable<Integer,Long> ht = new Hashtable<Integer,Long>(1024,0.75f);
-			
-			/*
-			 * for each type of hash table, put, get, remove many pairs, N, K times. Use wall clock
-			 * time to measure performance.
-			 */
-			/*
-			 * first run the FastHashtable.
-			 */
-			start_time = new Date();
-			for(int j=0;j<K;++j) {
-				for(int i=1;i<N;++i) {
-					ft.put(i, i);
-					s += ft.get(i);
-				}
-				assert ft.size()==(N-1); //test the size for this very large object.
-				for(int i=1;i<N;++i) {
-					ft.remove(i);
-				}
-				assert ft.size()==0;
-			}
-			stop_time = new Date();
-			ft_total_time += (stop_time.getTime()-start_time.getTime());
-		
-			/*
-			 * now run the java.util.Hashtable.
-			 */
-			start_time = new Date();
-			for(int j=0;j<K;++j) {
-				for(int i=1;i<N;++i) {
-					Long v = new Long(i);
-					ht.put(i, v);
-					s += ht.get(i);
-				}
-				for(int i=1;i<N;++i) {
-					ht.remove(i);
-				}
-			}
-			stop_time = new Date();
-			
-			/*
-			 * compare the elapsed times.
-			 */
-			ht_total_time += (stop_time.getTime()-start_time.getTime());
-			System.out.println("runtime comparison: ft: "+ft_total_time+"ms, ht: "+ht_total_time+"ms, ft/ht: "+(ft_total_time/ht_total_time));
-			return ft_total_time < ht_total_time;
-		}
-		
-	}
-	
-
-
-	/*
 	 * Returns the list of unit tests to run. Add new tests here.
 	 */
 	public ArrayList<TestAbstract> getTestsList() {
@@ -193,7 +124,6 @@ public class FHTest {
 		tests.add(new TestPut());
 		tests.add(new TestCollision());
 		tests.add(new TestForMissingKey());
-  		tests.add(new TestRelativeComputationTime());
 		
 		return tests;
 		
